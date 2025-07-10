0ca41934fec3d64495744abbd567f1937a72e3be
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-07-10 13:19:01 +0300

    Update problem1.c

--- 06/solutions/problem1.c
+++ 06/solutions/problem1.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include<unistd.h>
 int main(int argc, char *argv[]) {
     int arr[2];
@@ -6,9 +5,8 @@ int main(int argc, char *argv[]) {
         return 1;
     close(arr[1]);
     char buf[10];
-    int read_res = read(arr[0], buf, 10);
-    printf("%d\n", read_res);
-    write(1, buf, read_res);
+    int r = read(arr[0], buf, 10);
+    write(1, buf, r);
     close(arr[0]);
     return 0;
 }
