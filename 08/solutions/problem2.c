9b20ba2b11cb1007e0834e2830200a0f0d193d91
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-07-23 23:55:11 +0100

    Update problem2.c

--- 08/solutions/problem2.c
+++ 08/solutions/problem2.c
@@ -1,22 +1,17 @@
+#include<signal.h>
 #include<unistd.h>
 #include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<signal.h>
-void func(int sig_val) {
-    if (sig_val == 13)
-        printf("Signal\n");
+void f(int sig_val) {
+	if (sig_val == SIGPIPE)
+		printf("Sigpipe\n");
 }
 int main() {
-    signal(13, func);
-    int arr[2];
-    if (pipe(arr) == -1)
-        return 1;
-    close(arr[0]);
-    int w = write(arr[1], "tester\n", 7);
-    if (w == -1)
-        return 1;
-    printf("%d\nbuffer\n", w);
-    close(arr[1]);
-    return 0;
+	signal(SIGPIPE, f);
+	int arr[2];
+	if (pipe(arr) == -1)
+		return 1;
+	close(arr[0]);
+	write(arr[1], "test\n", 5);
+	close(arr[1]);
+	return 0;
 }
