3daf1a1046586238062003be76da7079ab4b288f
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-07-23 23:58:50 +0100

    Update problem5.c

--- 08/solutions/problem5.c
+++ 08/solutions/problem5.c
@@ -1,25 +1,20 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
 #include<signal.h>
-unsigned long int count = 0;
-void f0(int sig_val) {
-    if (sig_val == SIGALRM) {
-        printf("%ld\n", count);
-        alarm(1);
-    }
-}
-void f1(int sig_val) {
-    if (sig_val == SIGINT)
-        count = 0;
+#include<unistd.h>
+#include<stdio.h>
+long unsigned int c = 0;
+void f(int sig_val) {
+	if (sig_val == 14) {
+		printf("%ld\n", c);
+		alarm(1);
+	}
+	else if (sig_val == SIGINT)
+		c = 0;
 }
 int main() {
-    int i;
-    alarm(1);
-    for (i = 0; i < 10000000; i++) {
-        count++;
-        signal(SIGALRM, f0);
-        signal(SIGINT, f1);
-    }
-    return 0;
+	alarm(1);
+	while (1) {
+		signal(SIGALRM, f);
+		signal(SIGINT, f);
+		c++;
+	}
 }
