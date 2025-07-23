55fd8a93c152d4a6f4c01a90a4986a297e65f561
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-07-23 23:54:28 +0100

    Update problem1.c

--- 08/solutions/problem1.c
+++ 08/solutions/problem1.c
@@ -1,18 +1,12 @@
-#include<unistd.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
 #include<signal.h>
+#include<unistd.h>
 int main() {
-    signal(13, SIG_IGN);
-    int arr[2];
-    if (pipe(arr) == -1)
-        return 1;
-    close(arr[0]);
-    int w = write(arr[1], "tester\n", 7);
-    if (w == -1)
-        return 1;
-    printf("%d\nbuffer\n", w);
-    close(arr[1]);
-    return 0;
+	signal(SIGPIPE, SIG_IGN);
+	int arr[2];
+	if (pipe(arr) == -1)
+		return 1;
+	close(arr[0]);
+	write(arr[1], "test\n", 5);
+	close(arr[1]);
+	return 0;
 }
