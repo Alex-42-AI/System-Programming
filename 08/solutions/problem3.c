de8facf42162094569d1e3fdf21c363a70508296
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-07-23 23:56:52 +0100

    Update problem3.c

--- 08/solutions/problem3.c
+++ 08/solutions/problem3.c
@@ -1,26 +1,24 @@
+#include<signal.h>
 #include<stdio.h>
 #include<unistd.h>
-#include<stdlib.h>
 #include<sys/types.h>
-#include<signal.h>
 void func(int sig_val) {
-    if (sig_val == 17)
-        printf("Done\n");
-    else
-        printf("tf???\n");
+	if (sig_val == SIGCHLD)
+		printf("child\n");
 }
 int main() {
-    int f = fork();
-    if (f == -1)
-        return 1;
-    if (f) {
-        signal(17, func);
-        pause();
-        printf("parent.\n");
-    }
-    else {
-        printf("child...\n");
-        sleep(3);
-    }
-    return 0;
+	signal(17, func);
+	int f = fork();
+	if (f == -1)
+		return 1;
+	if (f) {
+		printf("parent...\n");
+		pause();
+		printf("parent\n");
+	}
+	else {
+		printf("child...\n");
+		sleep(2);
+	}
+	return 0;
 }
