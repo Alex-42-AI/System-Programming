e1383debf805e001654aa04fd4e25c4c5d8843c3
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-08 00:54:39 +0300

    Update problem1.c

--- 09/solutions/problem1.c
+++ 09/solutions/problem1.c
@@ -2,18 +2,17 @@
 #include<fcntl.h>
 #include<mqueue.h>
 int main() {
-    int q = mq_open("/unique_name", O_CREAT | O_RDONLY | O_EXCL, 0644, NULL);
+    int q = mq_open("/prob1", O_CREAT | O_RDONLY | O_EXCL, 0644, NULL);
     if (q == -1)
         return 1;
     struct mq_attr mq;
     if (mq_getattr(q, &mq) == -1) {
         mq_close(q);
-        mq_unlink("/unique_name");
+        mq_unlink("/prob1");
         return 1;
     }
     printf("%d, %d, %d, %d\n", mq.mq_flags, mq.mq_maxmsg, mq.mq_msgsize, mq.mq_msgsize);
     mq_close(q);
-    mq_unlink("/unique_name");
+    mq_unlink("/prob1");
     return 0;
-
 }
