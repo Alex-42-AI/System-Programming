2553d550d22d9baee3a1e47bd04fec89d986917f
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-08 01:03:53 +0300

    Update problem5b.c

--- 09/solutions/problem5b.c
+++ 09/solutions/problem5b.c
@@ -1,22 +1,22 @@
 #include<mqueue.h>
 #include<unistd.h>
 #include<fcntl.h>
-#include<sys/types.h>
-#include<sys/stat.h>
 #include"problem5.h"
 int main(int argc, char *argv[]) {
     if (argc < 2)
         return 1;
     struct mq_attr mq = {.mq_maxmsg = 10, .mq_msgsize = 1};
-    int q = mq_open(Q_NAME, O_RDONLY, 0644, &mq);
+    int q = mq_open(Q_NAME, O_RDONLY, &mq);
     if (q == -1)
         return 1;
     while (1) {
         char buf[1];
-        int m = mq_receive(q, buf, 1, NULL);
-        if (!m || m == -1)
+        int r = mq_receive(q, buf, 1, NULL);
+        if (!r || r == -1)
             break;
         write(1, buf, 1);
+        if (buf[0] == '\0')
+            break;
     }
     mq_close(q);
     mq_unlink(Q_NAME);
