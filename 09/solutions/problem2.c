3184af9639bb2cbd9e3968934a7fffb008d43a84
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-08 00:56:02 +0300

    Update problem2.c

--- 09/solutions/problem2.c
+++ 09/solutions/problem2.c
@@ -1,27 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
 #include<fcntl.h>
-#include<sys/stat.h>
 #include<mqueue.h>
 int main() {
-    struct mq_attr mq = {.mq_maxmsg = 1, .mq_msgsize = 10};
-    int q = mq_open("/unique_name", O_RDWR | O_CREAT | O_EXCL, 0644, &mq);
+    struct mq_attr mq = {.mq_maxmsg = 10, .mq_msgsize = 1};
+    int q = mq_open("/prob2", O_RDWR | O_CREAT | O_EXCL, 0644, &mq);
     if (q == -1)
         return 1;
-    if (mq_send(q, "problem 2", 9, NULL) == -1) {
+    if (mq_send(q, "problem 2", 9, (unsigned)1) == -1) {
         mq_close(q);
-        mq_unlink("/unique_name");
+        mq_unlink("/prob2");
         return 1;
     }
     while (1) {
         char buf[1];
-        int m = mq_receive(q, buf, 1, NULL);
-        if (!m || m == -1)
+        int r = mq_receive(q, buf, 1, NULL);
+        if (!r || r == -1)
             break;
         write(1, buf, 1);
+        if (buf[0] == '\0')
+            break;
     }
     mq_close(q);
-    mq_unlink("/unique_name");
+    mq_unlink("/prob2");
     return 0;
-
 }
