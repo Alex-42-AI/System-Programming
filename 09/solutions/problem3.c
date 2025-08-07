5d275aeb2de0aee9e862bcb2f38733bc354ae586
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-08 00:57:11 +0300

    Update problem3.c

--- 09/solutions/problem3.c
+++ 09/solutions/problem3.c
@@ -1,4 +1,3 @@
-#include<sys/wait.h>
 #include<sys/types.h>
 #include<fcntl.h>
 #include<unistd.h>
@@ -6,39 +5,40 @@
 int main(int argc, char *argv[]) {
     if (argc < 2)
         return 1;
-    struct mq_attr mq = {.mq_maxmsg = 1, .mq_msgsize = 10};
-    int q = mq_open("/unique_name", O_CREAT | O_EXCL | O_RDWR, 0644, &mq);
+    struct mq_attr mq = {.mq_maxmsg = 10, .mq_msgsize = 1};
+    int q = mq_open("/prob3", O_CREAT | O_EXCL | O_RDWR, 0644, &mq);
     if (q == -1)
         return 1;
     int f = fork();
     if (f == -1) {
         mq_close(q);
-        mq_unlink("/unique_name");
+        mq_unlink("/prob3");
         return 1;
     }
     if (f) {
+        while (1) {
+            char buf[1];
+            int r = mq_receive(q, buf, 1, NULL);
+            if (!r || r == -1)
+                break;
+            write(1, buf, 1);
+            if (buf[0] == '\0')
+                break;
+        }
+        mq_close(q);
+        mq_unlink("/prob3");
+    }
+    else {
         int fd = open(argv[1], O_RDONLY);
         while (1) {
             char buf[1];
             int r = read(fd, buf, 1);
             if (!r || r == -1)
                 break;
-            mq_send(q, buf, 1, NULL);
+            mq_send(q, buf, 1, (unsigned)1);
         }
+        mq_send(q, "\0", (unsigned)1);
         close(fd);
     }
-    else {
-        waitpid(getppid());
-        while (1) {
-            char buf[1];
-            int m = mq_receive(q, buf, 1, NULL);
-            if (!m || m == -1)
-                break;
-            write(1, buf, 1);
-        }
-    }
-    mq_close(q);
-    mq_unlink("/unique_name");
     return 0;
-
 }
