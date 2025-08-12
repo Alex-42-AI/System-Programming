008ecd56e1697e6d2d7ae858c1876bcc4d09138c
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-12 21:38:07 +0300

    Update problem1.c

--- 10/solutions/problem1.c
+++ 10/solutions/problem1.c
@@ -5,14 +5,30 @@
 #include<unistd.h>
 #include<string.h>
 int main() {
-    int md = shm_open("/prob1", O_CREAT | O_EXCL | O_RDWR, 0777);
+    int md = shm_open("/prob1", O_CREAT | O_EXCL | O_RDWR, 0644);
     if (md == -1)
         return 1;
-    ftruncate(md, 8);
-    char *addr = mmap(NULL, 8, PROT_READ | PROT_WRITE, MAP_PRIVATE, md, 0);
-    strcpy(addr, "alphabet");
-    printf("%s\n", addr);
+    ftruncate(md, 11);
+    char *addr0 = mmap(NULL, 11, PROT_WRITE, MAP_PRIVATE, md, 0);
+    if (addr0 == MAP_FAILURE) {
+        close(md);
+        shm_unlink("/prob1");
+        return 1;
+    }
+    strcpy(addr0, "hello there");
+    char *addr1 = mmap(NULL, 11, PROT_WRITE, MAP_PRIVATE, md, 0);
+    if (addr1 == MAP_FAILURE) {
+        close(md);
+        shm_unlink("/prob1");
+        return 1;
+    }
+    printf("%s\n", addr1);
+    if (munmap(addr0, 11) == -1) {
+        close(md);
+        shm_unlink("/prob1");
+        return 1;
+    }
     close(md);
     shm_unlink("/prob1");
-    return 0;
+    return munmap(addr1, 11) == -1;
 }
