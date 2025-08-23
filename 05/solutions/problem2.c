43546e529f7155cce06367afac5a61e946740f09
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-23 10:53:56 +0100

    Update problem2.c

--- 05/solutions/problem2.c
+++ 05/solutions/problem2.c
@@ -1,6 +1,5 @@
 #include<fcntl.h>
 #include<unistd.h>
-#include<stdlib.h>
 #include<sys/stat.h>
 #include<sys/types.h>
 int main(int argc, char *argv[]) {
@@ -15,17 +14,16 @@ int main(int argc, char *argv[]) {
     if (f) {
         int i;
         char *bufs[4] = {"012", "23", "345", "56"};
-        for (i = 0; i < 4; i++) {
+        for (i = 0; i < 4; i++)
             write(fd, bufs[i], sizeof(bufs[i]));
-        }
     }
     else {
         int i;
         char *bufs[5] = {"abc", "cd", "def", "fg", "ghi"};
-        for (i = 0; i < 5; i++) {
+        for (i = 0; i < 5; i++)
             write(fd, bufs[i], sizeof(bufs[i]));
-        }
     }
     close(fd);
     return 0;
+
 }
