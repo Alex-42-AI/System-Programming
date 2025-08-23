5a2c3d73a2d893395071d3518d4bf180a21efb8e
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-23 15:29:42 +0100

    Update problem1.c

--- 13/solutions/problem1.c
+++ 13/solutions/problem1.c
@@ -1,28 +1,27 @@
 #include<signal.h>
-#include<unistd.h>
 #include<stdio.h>
+#include<unistd.h>
 #include<sys/types.h>
 void func(int _) {}
 int main() {
-    signal(SIGUSR1, func);
-    signal(SIGUSR2, func);
-    int f = fork();
-    if (f == -1)
-        return 1;
-    if (f) {
-        sleep(1);
-        printf("parent\n");
-        fflush(stdout);
-        kill(f, SIGUSR2);
-        pause();
-        printf("parent\n");
-        fflush(stdout);
-    }
-    else {
-        pause();
-        printf("child\n");
-        fflush(stdout);
-        kill(getppid(), SIGUSR1);
-    }
-    return 0;
+	signal(SIGTERM, func);
+	int f = fork();
+	if (f == -1)
+		return 1;
+	if (f) {
+		sleep(1);
+		printf("parent\n");
+		fflush(stdout);
+		kill(f, SIGTERM);
+		pause();
+		printf("parent\n");
+		fflush(stdout);
+	}
+	else {
+		pause();
+		printf("child\n");
+		fflush(stdout);
+		kill(getppid(), SIGTERM);
+	}
+	return 0;
 }
