84258b0d300f6e18d10338ff3cd56dea370ea617
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-23 11:34:34 +0100

    Update problem5.c

--- 06/solutions/problem5.c
+++ 06/solutions/problem5.c
@@ -1,12 +1,9 @@
 #include<unistd.h>
 #include<stdio.h>
-#include<fcntl.h>
-#include<stdlib.h>
 #include<sys/types.h>
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
+    if (argc < 3)
         return 1;
-    }
     int arr[2], f;
     if (pipe(arr) == -1)
         return 1;
@@ -25,15 +22,13 @@ int main(int argc, char *argv[]) {
         execlp(argv[1], argv[1], NULL);
         return 1;
     }
-    else {
-        close(arr[1]);
-        if (dup2(arr[0], 0) == -1) {
-            close(arr[0]);
-            return 1;
-        }
+    waitpid(f, NULL, 0);
+    close(arr[1]);
+    if (dup2(arr[0], 0) == -1) {
         close(arr[0]);
-        execlp(argv[2], argv[2], NULL);
         return 1;
     }
-    return 0;
+    close(arr[0]);
+    execlp(argv[2], argv[2], NULL);
+    return 1;
 }
