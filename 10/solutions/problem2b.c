6efe2390101c6096f35086d1d61f227aa5605595
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-23 15:16:00 +0100

    Update problem2b.c

--- 10/solutions/problem2b.c
+++ 10/solutions/problem2b.c
@@ -19,5 +19,24 @@ int main() {
     close(md);
     shm_unlink("/prob2");
     return munmap(addr, 8) == -1;
+}#include<sys/mman.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdio.h>
+int main() {
+	int md = shm_open("/prob2", O_RDONLY, 0);
+	if (md == -1) {
+		shm_unlink("/prob2");
+		return 1;
+	}
+	char *addr = mmap(NULL, 5, PROT_READ, MAP_SHARED, md, 0);
+	if (addr == MAP_FAILED) {
+		close(md);
+		shm_unlink("/prob2");
+		return 1;
+	}
+	printf("%s\n", addr);
+	close(md);
+	shm_unlink("/prob2");
+	return munmap(addr, 5) == -1;
 }
-
