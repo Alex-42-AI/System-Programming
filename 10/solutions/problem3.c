ca1eab1ad6e1d2a605d8162dc3ceaa032c6a8243
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-23 15:18:13 +0100

    Update problem3.c

--- 10/solutions/problem3.c
+++ 10/solutions/problem3.c
@@ -1,49 +1,47 @@
 #include<sys/mman.h>
-#include<sys/types.h>
-#include<stdlib.h>
-#include<fcntl.h>
-#include<stdio.h>
+#include<sys/stat.h>
 #include<unistd.h>
-#include<string.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<stdio.h>
 int main() {
-    int md = shm_open("/prob3", O_CREAT | O_EXCL | O_RDWR, 0644);
-    if (md == -1)
-        return 1;
-    if (ftruncate(md, sizeof(int)) == -1) {
-        close(md);
-        shm_unlink("/prob3");
-        return 1;
-    }
-    int *addr = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, md, 0);
-    if (addr == MAP_FAILED) {
-        close(md);
-        shm_unlink("/prob3");
-        return 1;
-    }
-    *addr = 0;
-    int f = fork();
-    if (f == -1) {
-        close(md);
-        shm_unlink("/prob3");
-        return 1;
-    }
-    if (f) {
-        sleep(1);
-        while (*addr < 100) {
-            *addr += 1;
-            printf("%d, %d\n", *addr, getpid());
-            sleep(2);
-        }
-    }
-    else {
-        while (*addr < 100) {
-            *addr += 1;
-            printf("%d, %d\n", *addr, getpid());
-            sleep(2);
-        }
-    }
-    close(md);
-    shm_unlink("/prob3");
-    return munmap(addr, sizeof(int)) == -1;
+	int md = shm_open("/prob3", O_CREAT | O_EXCL | O_RDWR, 0644);
+	if (md == -1)
+		return 1;
+	if (ftruncate(md, sizeof(int)) == -1) {
+		close(md);
+		shm_unlink("/prob3");
+		return 1;
+	}
+	int *addr = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, md, 0);
+	if (addr == MAP_FAILED) {
+		close(md);
+		shm_unlink("/prob3");
+		return 1;
+	}
+	*addr = 0;
+	int f = fork();
+	if (f == -1) {
+		close(md);
+		shm_unlink("/prob3");
+		return 1;
+	}
+	if (f) {
+		sleep(1);
+		while (*addr < 100) {
+			printf("%d, %d\n", *addr, getpid());
+			(*addr)++;
+			sleep(2);
+		}
+	}
+	else {
+		while (*addr < 100) {
+			printf("%d, %d\n", *addr, getpid());
+			(*addr)++;
+			sleep(2);
+		}
+	}
+	close(md);
+	shm_unlink("/prob3");
+	return munmap(addr, sizeof(int)) == -1;
 }
-
