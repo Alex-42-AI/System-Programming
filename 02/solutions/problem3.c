9602d680efa266cad24583cc8cdf26aa7cd3b2c5
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-23 10:04:21 +0100

    Update problem3.c

--- 02/solutions/problem3.c
+++ 02/solutions/problem3.c
@@ -1,15 +1,18 @@
-#include<sys/stat.h>
-#include<stdio.h>
 #include<fcntl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<stdio.h>
 int main(int argc, char *argv[]) {
-    int i;
-    for (i = 1; i < argc; i++) {
-        int fd = open(argv[i], O_RDONLY);
-        if (fd == -1)
-            continue;
-        int s = fstat(fd, NULL);
-        printf("%d\n", s);
-        close(fd);
-    }
-    return 0;
+	int i;
+	for (i = 1; i < argc; i++) {
+		int fd = open(argv[i], O_RDONLY);
+		if (fd == -1)
+			continue;
+		struct stat st;
+		int s = fstat(fd, &st);
+		printf("%ld %ld %ld\n", st.st_dev, st.st_ino, st.st_nlink);
+		close(fd);
+	}
+	return 0;
 }
