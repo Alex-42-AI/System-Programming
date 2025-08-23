5f3a6e2cd487b5f2ebdc79178ebbb5ae6a492e39
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-23 14:51:23 +0100

    Update problem5.c

--- 08/solutions/problem5.c
+++ 08/solutions/problem5.c
@@ -3,14 +3,14 @@
 #include<stdio.h>
 long unsigned int c = 0;
 void f(int sig_val) {
-	if (sig_val == 14) {
+	if (sig_val == SIGALRM) {
 		printf("%ld\n", c);
 		alarm(1);
 	}
 	else if (sig_val == SIGINT)
 		c = 0;
 }
-int main() {
+void main() {
 	alarm(1);
 	while (1) {
 		signal(SIGALRM, f);
