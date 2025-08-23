160ca92ce58eac82e42afd5ec0671cc3cc5fde3c
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-23 15:34:15 +0100

    Update problem3.c

--- 13/solutions/problem3.c
+++ 13/solutions/problem3.c
@@ -1,43 +1,51 @@
-#include<fcntl.h>
-#include<sys/stat.h>
-#include<unistd.h>
-#include<sys/mman.h>
 #include<signal.h>
+#include<sys/mman.h>
+#include<unistd.h>
+#include<fcntl.h>
 #include<stdio.h>
-volatile sig_atomic_t run = 1;
-void func(int sig_val) {
-    if (sig_val == SIGALRM) {
-        run = 0;
-    }
+volatile sig_atomic_t cont = 1;
+void handler(int sig_val) {
+	if (sig_val == SIGALRM)
+		cont = 0;
 }
 int main() {
-    int md = shm_open("/prob3", O_CREAT | O_EXCL | O_RDWR, 0644);
-    if (md == -1)
-        return 1;
-    signal(SIGALRM, func);
-    ftruncate(md, sizeof(int));
-    int *addr = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, md, 0);
-    *addr = 0;
-    int f = fork();
-    if (f == -1) {
-        close(md);
-        shm_unlink("/prob3");
-        return 1;
-    }
-    alarm(5);
-    if (f) {
-        while (run)
-            (*addr)++;
-        printf("parent: %d\n", *addr);
-    }
-    else {
-        while (run)
-            (*addr)--;
-        printf("child: %d\n", *addr);
-    }
-    munmap(addr, sizeof(int));
-    close(md);
-    if (f)
-        shm_unlink("/prob3");
-    return 0;
+	int md = shm_open("/prob3", O_CREAT | O_EXCL | O_RDWR, 0644);
+	if (md == -1)
+		return 1;
+	if (ftruncate(md, sizeof(int)) == -1) {
+		close(md);
+		shm_unlink("/prob3");
+		return 1;
+	}
+	int *addr = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, md, 0);
+	if (addr == MAP_FAILED) {
+		close(md);
+		shm_unlink("/prob3");
+		return 1;
+	}
+	signal(SIGALRM, handler);
+	*addr = 0;
+	int f = fork();
+	if (f == -1) {
+		close(md);
+		shm_unlink("/prob3");
+		munmap(addr, sizeof(int));
+		return 1;
+	}
+	if (f) {
+		alarm(5);
+		while (cont)
+			(*addr)++;
+		printf("%d %d\n", getpid(), *addr);
+	}
+	else {
+		alarm(5);
+		while (cont)
+			(*addr)--;
+		printf("%d %d\n", getpid(), *addr);
+	}
+	close(md);
+	if (f)
+		shm_unlink("/prob3");
+	return munmap(addr, sizeof(int)) == -1;
 }
