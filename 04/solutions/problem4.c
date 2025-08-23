410507b414467e9102a4e95b1c1064716bf63631
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-23 10:43:29 +0100

    Update problem4.c

--- 04/solutions/problem4.c
+++ 04/solutions/problem4.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<unistd.h>
-#include<stdlib.h>
 #include<sys/types.h>
 #include<sys/wait.h>
 int main(int argc, char *argv[]) {
@@ -31,4 +30,5 @@ int main(int argc, char *argv[]) {
     return 0;
 
 }
+
 
