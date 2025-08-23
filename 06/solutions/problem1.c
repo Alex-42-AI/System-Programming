b8be2db4873642dcdf9575dfcd26693a537c19a8
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-23 11:24:34 +0100

    Update problem1.c

--- 06/solutions/problem1.c
+++ 06/solutions/problem1.c
@@ -1,12 +1,16 @@
 #include<unistd.h>
-int main(int argc, char *argv[]) {
-    int arr[2];
-    if (pipe(arr) == -1)
-        return 1;
-    close(arr[1]);
-    char buf[10];
-    int r = read(arr[0], buf, 10);
-    write(1, buf, r);
-    close(arr[0]);
-    return 0;
+int main() {
+	int arr[2];
+	if (pipe(arr) == -1)
+		return 1;
+	close(arr[1]);
+	while (1) {
+		char buf[1];
+		int r = read(arr[0], buf, 1);
+		if (!r || r == -1)
+			break;
+		write(1, buf, 1);
+	}
+	close(arr[0]);
+	return 0;
 }
