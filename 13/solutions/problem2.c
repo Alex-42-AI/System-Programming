aaa62ca65cc9d99b79d21ea4d833a55522a05b30
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-23 15:31:43 +0100

    Update problem2.c

--- 13/solutions/problem2.c
+++ 13/solutions/problem2.c
@@ -1,36 +1,32 @@
 #include<mqueue.h>
-#include<fcntl.h>
-#include<stdio.h>
 #include<unistd.h>
+#include<stdio.h>
 int main() {
-    struct mq_attr mq;
-    mq.mq_maxmsg = 3;
-    mq.mq_msgsize = 2;
-    int q = mq_open("/prob2", O_CREAT | O_EXCL | O_RDWR, 0644, &mq);
-    if (q == -1)
-        return 1;
-    int f = fork();
-    if (f == -1) {
-        mq_close(q);
-        mq_unlink("/prob2");
-        return 1;
-    }
-    if (f) {
-        sleep(1);
-        printf("parent\n");
-        mq_send(q, "go", 2, 0);
-        char buf[2];
-        mq_receive(q, buf, 2, NULL);
-        printf("parent\n");
-        mq_close(q);
-        mq_unlink("/prob2");
-    }
-    else {
-        char buf[2];
-        mq_receive(q, buf, 2, NULL);
-        printf("child\n");
-        mq_send(q, "go", 2, 0);
-        mq_close(q);
-    }
-    return 0;
+	struct mq_attr mq;
+	mq.mq_maxmsg = 3;
+	mq.mq_msgsize = 2;
+	int md = mq_open("/prob2", O_CREAT | O_EXCL | O_RDWR, 0644, &mq);
+	int f = fork();
+	if (f == -1) {
+		mq_close(md);
+		mq_unlink("/prob2");
+	}
+	if (f) {
+		sleep(1);
+		printf("parent\n");
+		mq_send(md, "go", 2, 0);
+		char buf[2];
+		mq_receive(md, buf, 2, NULL);
+		printf("parent\n");
+	}
+	else {
+		char buf[2];
+		mq_receive(md, buf, 2, NULL);
+		printf("child\n");
+		mq_send(md, "go", 2, 0);
+	}
+	mq_close(md);
+	if (f)
+		mq_unlink("/prob2");
+	return 0;
 }
