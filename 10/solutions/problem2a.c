eaf548128b80cd28d1dff88363e2d0a2e7cda1d2
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-23 15:14:28 +0100

    Update problem2a.c

--- 10/solutions/problem2a.c
+++ 10/solutions/problem2a.c
@@ -1,31 +1,29 @@
 #include<sys/mman.h>
-#include<stdlib.h>
-#include<fcntl.h>
-#include<stdio.h>
-#include<string.h>
 #include<unistd.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<string.h>
 int main() {
-    int md = shm_open("/prob2", O_CREAT | O_EXCL | O_RDWR, 0644);
-    if (md == -1)
-        return 1;
-    if (ftruncate(md, 8) == -1) {
-        close(md);
-        shm_unlink("/prob2");
-        return 1;
-    }
-    char *addr = mmap(NULL, 8, PROT_READ | PROT_WRITE, MAP_SHARED, md, 0);
-    if (addr == MAP_FAILED) {
-        close(md);
-        shm_unlink("/prob2");
-        return 1;
-    }
-    strcpy(addr, "alphabet");
-    close(md);
-    if (munmap(addr, 8) == -1) {
-        shm_unlink("/prob2");
-        return 1;
-    }
-    return 0;
+	int md = shm_open("/prob2", O_CREAT | O_EXCL | O_RDWR, 0644);
+	if (md == -1)
+		return 1;
+	if (ftruncate(md, 5) == -1) {
+		close(md);
+		shm_unlink("/prob2");
+		return 1;
+	}
+	char *addr = mmap(NULL, 5, PROT_WRITE, MAP_SHARED, md, 0);
+	if (addr == MAP_FAILED) {
+		close(md);
+		shm_unlink("/prob2");
+		return 1;
+	}
+	strcpy(addr, "hello");
+	if (munmap(addr, 5) == -1) {
+		close(md);
+		shm_unlink("/prob2");
+		return 1;
+	}
+	close(md);
+	return 0;
 }
-
-
